d4eb3228b55b538ee8b21b258870bfb0ba9eadec
sklaw <sk.law.lsq@gmail.com>
2019-07-17 16:02:24 -0400

    saving

--- nodes/agent_topics_handler_node.py
+++ nodes/agent_topics_handler_node.py
@@ -3,7 +3,7 @@ import rospy
 from baxter_as_gps_ros_agent import AgentTopicsHandler
 
 if __name__ == '__main__':
-    rospy.init_node('agent_topics_handler_node')
+    rospy.init_node('agent_topics_handler_node', log_level=rospy.DEBUG)
     agent_topics_handler = AgentTopicsHandler()
     agent_topics_handler.setup_subscriber_and_publisher()
     rospy.loginfo("agent_topics_handler_node starts")
--- src/baxter_as_gps_ros_agent/agent_topics_handler.py
+++ src/baxter_as_gps_ros_agent/agent_topics_handler.py
@@ -8,19 +8,32 @@ roslib.load_manifest('gps_agent_pkg')
 
 from gps_agent_pkg.msg import (
     TrialCommand, 
-    SampleResult, 
     PositionCommand, 
     RelaxCommand, 
     DataRequest, 
-    TfActionCommand, 
-    TfObsData,
+    SampleResult, 
 )
-import trial_command_handler
 
 class AgentTopicsHandler(object):
     def __init__(self):
         pass
 
+    def trial_command_callback(self, trial_command):
+        rospy.logdebug('receive trial command: %s'%trial_command)
+
+    def position_command_callback(self, position_command):
+        rospy.logdebug('receive position command: %s'%position_command)
+
+    def relax_command_callback(self, relax_command):
+        rospy.logdebug('receive relax command: %s'%relax_command)
+
+    def data_request_callback(self, data_request):
+        rospy.logdebug('receive data request: %s'%data_request)
+
     def setup_subscriber_and_publisher(self):
-        rospy.Subscriber("gps_controller_trial_command", TrialCommand, trial_command_handler.callback)
+        self.trial_command_sub = rospy.Subscriber("gps_controller_trial_command", TrialCommand, self.trial_command_callback)
+        self.position_command_sub = rospy.Subscriber("gps_controller_position_command", PositionCommand, self.position_command_callback)
+        self.relax_command_sub = rospy.Subscriber("gps_controller_relax_command", RelaxCommand, self.relax_command_callback)
+        self.data_request_sub = rospy.Subscriber("gps_controller_data_request", DataRequest, self.data_request_callback)
+        self.sample_result_pub = rospy.Publisher("gps_controller_report", SampleResult, queue_size=None)
 
