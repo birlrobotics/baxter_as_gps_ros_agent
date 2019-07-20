eb1adced93d7500aa28a62062049f6a3f336f335
sklaw <sk.law.lsq@gmail.com>
2019-07-20 10:25:12 -0400

    implementing default config

--- config/default_config.py
+++ config/default_config.py
@@ -1,6 +1,27 @@
+# The original GPS package doesn't use catkin.
+# So the folloing lines are necessary if we want
+#   to import packages from gps_agent_pkg
+import roslib
+roslib.load_manifest('gps_agent_pkg')
+
+import os
+import gps_agent_pkg
+import sys
+sys.path.append(os.path.join(
+    os.sep.join(gps_agent_pkg.__path__[0].split(os.sep)[:-4]),
+    'python',
+))
+
 from gps.proto.gps_pb2 import JOINT_ANGLES, JOINT_VELOCITIES, \
         END_EFFECTOR_POINTS, END_EFFECTOR_POINT_VELOCITIES, ACTION
+from rostopics_to_timeseries import RosTopicFilteringScheme, OfflineRostopicsToTimeseries
+from baxter_as_gps_ros_agent.ros_topic_filters.right_arm_joint_angle_filter import RightArmJointAngleFilter
+from sensor_msgs.msg import JointState
 
-map_datatype_to_topic = {
-    ACTION: 'baxter_action',
+map_datatype_to_filter = {
+    JOINT_ANGLES: {
+        "topic_name": "/robot/joint_states",
+        "msg_type": JointState,
+        "filter_class": RightArmJointAngleFilter,
+    }
 }
